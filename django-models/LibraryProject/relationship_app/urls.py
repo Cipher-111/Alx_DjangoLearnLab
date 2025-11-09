b857d3adc63432de767a72eef7019038c7bebbd8
Sunday Onoja <sundayonoja23@gmail.com>
2025-11-09 01:17:15 +0100

    Fix urls for ALX checker 1

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -24,3 +24,4 @@ urlpatterns = [
     path('books/<int:book_id>/edit/', views.edit_book, name='edit_book'),
     path('books/<int:book_id>/delete/', views.delete_book, name='delete_book'),
 ]
+["add_book/", "edit_book/"]
