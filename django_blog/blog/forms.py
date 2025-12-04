a8125f507f056bdc1298cfffa9de2de4fc3097d8
Sunday Onoja <sundayonoja23@gmail.com>
2025-12-04 10:34:21 +0100

    recommitting

--- django_blog/blog/forms.py
+++ django_blog/blog/forms.py
@@ -2,6 +2,17 @@ from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 
+from django import forms
+from .models import Comment
+
+class CommentForm(forms.ModelForm):
+    content = forms.CharField(widget=forms.Textarea(attrs={'rows': 3}), label='')
+
+    class Meta:
+        model = Comment
+        fields = ['content']
+
+
 class RegisterForm(UserCreationForm):
     email = forms.EmailField(required=True)
     
