c0e87ad2214b4365264205698a562e43c4e24a54
Sunday Onoja <sundayonoja23@gmail.com>
2025-12-08 15:12:23 +0100

    Define URL Patterns

--- social_media_api/social_media_api/urls.py
+++ social_media_api/social_media_api/urls.py
@@ -17,6 +17,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from django.urls import path, include
+from django.contrib import admin
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/accounts/', include('accounts.urls')),
 ]
+
