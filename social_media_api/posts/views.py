803d288fbff35567a81ae949d9c37fa9e5592d92
Sunday Onoja <sundayonoja23@gmail.com>
2025-12-08 16:40:44 +0100

    Step 3: Create Views for CRUD Operations

--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -1,3 +1,37 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
+from .models import Post, Comment
+from .serializers import PostSerializer, CommentSerializer
 
-# Create your views here.
+# POSTS
+class PostListCreateView(generics.ListCreateAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)  # automatically set author
+
+class PostRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def perform_update(self, serializer):
+        serializer.save(author=self.request.user)
+
+# COMMENTS
+class CommentListCreateView(generics.ListCreateAPIView):
+    queryset = Comment.objects.all()
+    serializer_class = CommentSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
+
+class CommentRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Comment.objects.all()
+    serializer_class = CommentSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def perform_update(self, serializer):
+        serializer.save(author=self.request.user)
