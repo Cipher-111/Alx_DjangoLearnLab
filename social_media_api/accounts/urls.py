a578f90f93150ecabdf931db7fdc2c3cb1a87fd3
Sunday Onoja <sundayonoja23@gmail.com>
2025-12-09 11:11:02 +0100

    Define URL Patterns for New Features

--- social_media_api/accounts/urls.py
+++ social_media_api/accounts/urls.py
@@ -1,11 +1,22 @@
+# from django.urls import path
+# from .views import RegisterView, LoginView, ProfileView, FollowUserView, UnfollowUserView, FollowingListView
+
+# urlpatterns = [
+#     path('register/', RegisterView.as_view(), name='register'),
+#     path('login/', LoginView.as_view(), name='login'),
+#     path('profile/', ProfileView.as_view(), name='profile'),
+#     path('follow/<str:username>/', FollowUserView.as_view(), name='follow-user'),
+#     path('unfollow/<str:username>/', UnfollowUserView.as_view(), name='unfollow-user'),
+#     path('following/', FollowingListView.as_view(), name='following-list'),
+# ]
+
 from django.urls import path
-from .views import RegisterView, LoginView, ProfileView, FollowUserView, UnfollowUserView, FollowingListView
+from .views import RegisterView, LoginView, ProfileView, FollowUserView, UnfollowUserView
 
 urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
     path('login/', LoginView.as_view(), name='login'),
     path('profile/', ProfileView.as_view(), name='profile'),
-    path('follow/<str:username>/', FollowUserView.as_view(), name='follow-user'),
-    path('unfollow/<str:username>/', UnfollowUserView.as_view(), name='unfollow-user'),
-    path('following/', FollowingListView.as_view(), name='following-list'),
+    path('follow/<int:user_id>/', FollowUserView.as_view(), name='follow-user'),
+    path('unfollow/<int:user_id>/', UnfollowUserView.as_view(), name='unfollow-user'),
 ]
