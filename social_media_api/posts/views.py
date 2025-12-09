2d84f124866436be5d30f8f7d71be52d1265f0ad
Sunday Onoja <sundayonoja23@gmail.com>
2025-12-09 10:06:33 +0100

    Implement Pagination and Filtering

--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -1,27 +1,29 @@
-from rest_framework import viewsets, permissions
-from rest_framework.exceptions import PermissionDenied
+from rest_framework import viewsets, permissions, filters
+from django_filters.rest_framework import DjangoFilterBackend
 from .models import Post, Comment
 from .serializers import PostSerializer, CommentSerializer
 
-# Custom permission: only author can edit/delete
 class IsAuthorOrReadOnly(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
-        # Read permissions are allowed to any request
         if request.method in permissions.SAFE_METHODS:
             return True
-        # Write permissions only for author
         return obj.author == request.user
 
-# POST VIEWSET
+
 class PostViewSet(viewsets.ModelViewSet):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
     permission_classes = [permissions.IsAuthenticated, IsAuthorOrReadOnly]
 
+    # Add filtering + searching
+    filter_backends = [DjangoFilterBackend, filters.SearchFilter]
+    filterset_fields = ['title']     # exact filtering
+    search_fields = ['title', 'content']  # partial match search
+
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)
 
-# COMMENT VIEWSET
+
 class CommentViewSet(viewsets.ModelViewSet):
     queryset = Comment.objects.all()
     serializer_class = CommentSerializer
--- social_media_api/social_media_api/settings.py
+++ social_media_api/social_media_api/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'rest_framework.authtoken',
     'accounts',
     'posts',
+    'django_filters',
 ]
 
 MIDDLEWARE = [
@@ -121,4 +122,13 @@ USE_TZ = True
 STATIC_URL = 'static/'
 
 AUTH_USER_MODEL = 'accounts.User'
+
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10,
+    'DEFAULT_FILTER_BACKENDS': [
+        'django_filters.rest_framework.DjangoFilterBackend',
+        'rest_framework.filters.SearchFilter',
+    ],
+}
 
